645c639a0ed918562b4da44473aae2fbf89130dc
Juan <martin.gonzaless2005@gmail.com>
2025-11-12 12:13:54 -0500

    Se agregaro modulo de perfil de usuario

--- src/app/dashboard/doctor/perfil-doctor/perfil-doctor.ts
+++ src/app/dashboard/doctor/perfil-doctor/perfil-doctor.ts
@@ -1,14 +1,120 @@
 import { Component } from '@angular/core';
-import {Error} from '../../../info/pages/error/error';
+import { DoctorService } from '../../../core/services/doctor/doctor';
+import Swal from 'sweetalert2';
+import { NgClass } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+interface InfoDoctor {
+  id?: number;
+  nombre?: string;
+  apellido?: string;
+  edad?: number;
+  sexo?: string;
+  fechaNacimiento?: string;
+  telefono?: string;
+  email?: string;
+}
+
+interface CambiarPassword{
+  password?: string;
+}
 
 @Component({
   selector: 'app-perfil-doctor',
-  imports: [
-    Error
-  ],
+  imports: [NgClass, ReactiveFormsModule],
   templateUrl: './perfil-doctor.html',
   styleUrl: './perfil-doctor.css'
 })
 export class PerfilDoctor {
+
+  infoDoctor: InfoDoctor = {};
+  cambiarPassword: boolean = false;
+  seePassword: boolean = false;
+  seeConfirmPassword: boolean = false;
+  constructor(private doctorService: DoctorService){}
+
+  form = new FormGroup({
+    newPassword: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(8)
+      ]
+    }),
+    confirmPassword: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(8)
+      ]
+    }),
+  })
+
+
+  ngOnInit(){
+    this.cargarInfoPaciente();
+  }
+
+  cargarInfoPaciente(){
+    this.doctorService.obtenerPerfilDoctor().subscribe({
+      next: (info) => {
+        this.infoDoctor = info;
+        this.infoDoctor.sexo = info.sexo === 'M' ? 'Masculino' : 'Femenino';
+      }
+    })
+  }
+
+  actualizarPassword(){
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const newPassword : CambiarPassword = {
+      password: this.form.get('newPassword')?.value
+    }
+
+    this.doctorService.actualizarPassword(newPassword).subscribe({
+      next: (response) => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Contraseña actualizado',
+          showConfirmButton: false,
+          timer: 1500
+        });
+        this.cargarInfoPaciente();
+        this.cancelar()
+      },
+      error: (error) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error al actualizar el perfil',
+          text: error.error.message,
+          showConfirmButton: true,
+        });
+      }
+    })
+  }
+  togglePasswordVisibility(): void {
+    let password = document.getElementById('newPasswordPaciente') as HTMLInputElement
+    this.seePassword = !this.seePassword;
+    password.type = this.seePassword ? 'text' : 'password';
+  }
+  toggleConfirmPasswordVisibility(): void {
+    let password = document.getElementById('confirPasswordPaciente') as HTMLInputElement
+    this.seeConfirmPassword = !this.seeConfirmPassword;
+    password.type = this.seeConfirmPassword ? 'text' : 'password';
+  }
+
+  confirmarPassword():boolean {
+    const password = this.form.get('newPassword')?.value;
+    const confirmPassword = this.form.get('confirmPassword')?.value;
+    return password === confirmPassword;
+  }
+
+  cancelar(){
+    this.cambiarPassword = false
+    this.form.reset()
+  }
 
 }
--- src/app/dashboard/paciente/perfil-paciente/perfil-paciente.ts
+++ src/app/dashboard/paciente/perfil-paciente/perfil-paciente.ts
@@ -1,27 +1,142 @@
-import { Component } from '@angular/core';
-import {Error} from '../../../info/pages/error/error';
-import { LoginService } from '../../../core/services/auth/login/login';
+import { Component, NgModule } from '@angular/core';
+import { PacienteService } from '../../../core/services/paciente/paciente';
+import Swal from 'sweetalert2';
+import { NgClass } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+interface InfoPaciente {
+  id?: number;
+  dni?: string;
+  nombre?: string;
+  apellido?: string;
+  edad?: number;
+  sexo?: string;
+  fechaNacimiento?: string;
+  telefono?: string;
+  email?: string;
+}
+
+interface CambiarPassword{
+  password?: string;
+}
 
 @Component({
   selector: 'app-perfil-paciente',
-  imports: [ ],
+  imports: [NgClass, ReactiveFormsModule],
   templateUrl: './perfil-paciente.html',
   styleUrl: './perfil-paciente.css'
 })
 export class PerfilPaciente {
-  infoPaciente: any = {};
-  constructor(private loginService: LoginService){}
+  infoPaciente: InfoPaciente = {};
+
+  cambiarPassword: boolean = false;
+  seePassword: boolean = false;
+  seeConfirmPassword: boolean = false;
+  constructor(private pacienteService: PacienteService){}
+
+  form = new FormGroup({
+  newPassword: new FormControl('', {
+    nonNullable: true,
+    validators: [
+      Validators.required,
+      Validators.minLength(8)
+    ]
+  }),
+  confirmPassword: new FormControl('', {
+    nonNullable: true,
+    validators: [
+      Validators.required,
+      Validators.minLength(8)
+    ]
+    }),
+  })
+
 
   ngOnInit(){
-    const infoToken = this.loginService.getInfoToken();
-    this.infoPaciente = {
-      dni: infoToken.username,
-      nombre: infoToken.nombre,
-      apellido: infoToken.apellido,
-      email: infoToken.email,
-      telefono: infoToken.telefono
-    };
+    this.cargarInfoPaciente();
   }
 
+  cargarInfoPaciente(){
+    this.pacienteService.obtenerPerfilPaciente().subscribe({
+      next: (info) => {
+        this.infoPaciente = info;
+        this.infoPaciente.sexo = info.sexo === 'M' ? 'Masculino' : 'Femenino';
+      }
+    })
+  }
 
+  editarPerfil(){
+    this.pacienteService.actualizarPaciente(this.infoPaciente).subscribe({
+      next: (response) => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Perfil actualizado',
+          showConfirmButton: false,
+          timer: 1500
+        });
+        this.cargarInfoPaciente();
+      },
+      error: (error) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error al actualizar el perfil',
+          text: error.error.message,
+          showConfirmButton: false,
+          timer: 1500
+        });
+      }
+    })
+  }
+  actualizarPassword(){
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const newPassword : CambiarPassword = {
+      password: this.form.get('newPassword')?.value
+    }
+
+    this.pacienteService.actualizarPassword(newPassword).subscribe({
+      next: (response) => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Contraseña actualizado',
+          showConfirmButton: false,
+          timer: 1500
+        });
+        this.cargarInfoPaciente();
+        this.cancelar()
+      },
+      error: (error) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error al actualizar el perfil',
+          text: error.error.message,
+          showConfirmButton: true,
+        });
+      }
+    })
+  }
+  togglePasswordVisibility(): void {
+    let password = document.getElementById('newPasswordPaciente') as HTMLInputElement
+    this.seePassword = !this.seePassword;
+    password.type = this.seePassword ? 'text' : 'password';
+  }
+  toggleConfirmPasswordVisibility(): void {
+    let password = document.getElementById('confirPasswordPaciente') as HTMLInputElement
+    this.seeConfirmPassword = !this.seeConfirmPassword;
+    password.type = this.seeConfirmPassword ? 'text' : 'password';
+  }
+
+  confirmarPassword(): boolean {
+    const password = document.getElementById('newPasswordPaciente') as HTMLInputElement;
+    const confirmPassword = document.getElementById('confirPasswordPaciente')as HTMLInputElement;
+    return password.value === confirmPassword.value;
+  }
+
+  cancelar(){
+    this.cambiarPassword = false
+    this.form.reset()
+  }
 }
